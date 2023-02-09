3a869dd564660ab5576c9b964fd9eb461dedcdb5
DenysMkl <den.mikliaiev@gmail.com>
2023-02-09 23:35:35 +0300

    final satge

---
+++ js_code/script.js
@@ -0,0 +1,21 @@
+function Info() {
+    if (document.getElementById("change_block").style.display=="flex") {
+        document.getElementById("change_block").style.display="none";
+
+    }
+    else{
+        document.getElementById("change_block").style.display="flex";
+        document.getElementById("change_block").style.backgroundColor="#21244b"
+        var height = 0
+        var id = setInterval(frame, 10)
+        function frame(){
+            if (height >= 270){
+                clearInterval
+            }
+            else{
+                height += 5
+                document.getElementById("change_block").style.height = height+'px'
+            }
+        }
+    }
+}
