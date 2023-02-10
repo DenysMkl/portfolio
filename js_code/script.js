c10d0a7f5e12f2514ad4ae78b553bd29a74d637d
DenysMkl <den.mikliaiev@gmail.com>
2023-02-10 15:29:16 +0300

    финальная стадия создания шапки сайта

--- js_code/script.js
+++ js_code/script.js
@@ -1,8 +1,15 @@
-var opener = document.querySelector('.x_bar')
-
+var opener = document.querySelector('.phone_menu')
+var perem = document.querySelector('.x_bar')
+var sub_menu = document.querySelectorAll('.items')
 var menu = document.querySelector('.change_block')
 
 opener.addEventListener('click', function () {
-    opener.classList.toggle('active')
+    perem.classList.toggle('active')
     menu.classList.toggle('active')
+})
+
+sub_menu.forEach(elem => {
+    elem.addEventListener('click', function(){
+        elem.classList.toggle('active')
+    })
 })
