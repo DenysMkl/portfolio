94f8d2f490906151b27b2aab457b99543f323823
DenysMkl <den.mikliaiev@gmail.com>
2023-02-13 22:19:51 +0200

    add light and night theme

--- js_code/script.js
+++ js_code/script.js
@@ -2,6 +2,8 @@ var opener = document.querySelector('.phone_menu')
 var perem = document.querySelector('.x_bar')
 var sub_menu = document.querySelectorAll('.items')
 var menu = document.querySelector('.change_block')
+var theme = document.querySelector('.theme')
+var header = document.querySelector('.header')
 
 opener.addEventListener('click', function () {
     perem.classList.toggle('active')
@@ -12,4 +14,21 @@ sub_menu.forEach(elem => {
     elem.addEventListener('click', function(){
         elem.classList.toggle('active')
     })
+})
+/*
+logo.addEventListener('click', function () {
+    logo.classList.toggle('active')
+    if(logo.classList.contains('active')){
+        sub_menu[0].textContent = "Главная"
+    }
+    else{
+        sub_menu[0].textContent = "Home"
+    }
+    
+})*/
+
+theme.addEventListener('click', function () {
+    theme.classList.toggle('night')
+    theme.classList.toggle('light')
+    header.classList.toggle('active')
 })
