9c00b0804474855d7c4cde01ecfc544c77e2360d
DenysMkl <den.mikliaiev@gmail.com>
2023-02-21 21:41:13 +0200

    add some features for search_bar

--- js_code/script.js
+++ js_code/script.js
@@ -7,6 +7,9 @@ var header = document.querySelector('.header')
 var footer = document.querySelector('.footer')
 var avatar = document.querySelector('.profile')
 var prof_menu = document.querySelector('.profile_info')
+var search_bar = document.querySelector('.search_bar')
+var prodNames = Array.from(document.querySelectorAll('.prodName'))
+
 
 
 avatar.addEventListener('click', function () {
@@ -37,4 +40,23 @@ document.addEventListener('click', function (event) {
     if ((!prof_menu.contains(event.target) && prof_menu.classList.contains('open'))&& !avatar.contains(event.target)){
         prof_menu.classList.remove('open')
     }
+})
+
+var inter = null
+
+
+search_bar.addEventListener('focus', function () {
+    inter = setInterval(function () {
+        var mass = []
+        prodNames.forEach(el =>{
+            if (el.textContent.search(search_bar.value) != -1){
+                mass.push(el.textContent)
+            }
+        })
+        console.log(mass)
+    }, 2000)
+})
+
+search_bar.addEventListener('blur', function () {
+    clearInterval(inter)
 })
