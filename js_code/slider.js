6a178f8a93fa880fec0794b797428e363ccf48fc
DenysMkl <den.mikliaiev@gmail.com>
2023-02-24 15:59:54 +0200

    add slider

--- js_code/slider.js
+++ js_code/slider.js
@@ -1,11 +1,28 @@
 var but = document.querySelectorAll('.arrow')
 var counter = 0
 var img_list = document.querySelector('.slide_photos')
-
 var img_len = document.querySelectorAll('.image_slide')
+var ul = document.querySelector('.slider_line')
+
+for (i = 0; i < img_len.length; i++){
+    var li = document.createElement('li')
+    li.classList.add('line')
+    ul.appendChild(li)
+}
+
+var list = document.querySelectorAll('.line')
+list[counter%img_len.length].classList.add('active')
+
+function removeAll(l) {
+    l.forEach(elem =>{
+        elem.classList.remove('active')
+    })
+}
 
 let intervalId = setInterval(()=>{
     counter+=1
+    removeAll(list)
+    list[counter%img_len.length].classList.add('active')
     img_list.style.left = -100 * (counter%img_len.length) + '%'
 }, 5000)
 
@@ -14,8 +31,11 @@ but.forEach(elem =>{
         clearInterval(intervalId)
         intervalId = setInterval(()=>{
             counter+=1
+            removeAll(list)
+            list[counter%img_len.length].classList.add('active')
             img_list.style.left = -100 * (counter%img_len.length) + '%'
         }, 5000)
+
         if (elem.classList.contains('left')){
             counter-=1
             if (counter < 0) {
@@ -25,6 +45,8 @@ but.forEach(elem =>{
         if (elem.classList.contains('right')) {
             counter+=1
         }
+        removeAll(list)
+        list[counter%img_len.length].classList.add('active')
         img_list.style.left = -100 * (counter%img_len.length) + '%'
     })
 })
