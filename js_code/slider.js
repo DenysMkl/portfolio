e9e1b10cf38d65a237afe86031a4bebb993e16c1
DenysMkl <den.mikliaiev@gmail.com>
2023-02-24 12:51:45 +0200

    update slider features

--- js_code/slider.js
+++ js_code/slider.js
@@ -4,10 +4,18 @@ var img_list = document.querySelector('.slide_photos')
 
 var img_len = document.querySelectorAll('.image_slide')
 
-
+let intervalId = setInterval(()=>{
+    counter+=1
+    img_list.style.left = -100 * (counter%img_len.length) + '%'
+}, 5000)
 
 but.forEach(elem =>{
     elem.addEventListener('click', function () {
+        clearInterval(intervalId)
+        intervalId = setInterval(()=>{
+            counter+=1
+            img_list.style.left = -100 * (counter%img_len.length) + '%'
+        }, 5000)
         if (elem.classList.contains('left')){
             counter-=1
             if (counter < 0) {
@@ -17,6 +25,6 @@ but.forEach(elem =>{
         if (elem.classList.contains('right')) {
             counter+=1
         }
-        img_list.style.left = -100 * (counter%img_len.length) + '%'      
+        img_list.style.left = -100 * (counter%img_len.length) + '%'
     })
 })
